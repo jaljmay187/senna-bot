5bc6fd5253d2c72fc08b75d55cf136ed7a4ee337
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-14 21:16:22 +0300

    Update fun-simsimi.js

--- plugins/fun-simsimi.js
+++ plugins/fun-simsimi.js
@@ -2,9 +2,8 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
- let lang = global.db.data.users[m.sender].language
-  if (!text) throw `✳️ ${mssg.notext}`
-  m.react('🗣️') 
+ let handler = async (m, { text, command, args, usedPrefix }) => {
+  if (!text) throw `اكتب نصا للتحدث معي\nمثال: ${usedPrefix + command} مرحبا*`
   try { 
   //let res = await fetch(`https://api.simsimi.vn/v2/?text=${text}&lc=${lang}`)
   let res = await fetch('https://api.simsimi.vn/v1/simtalk', {
@@ -15,7 +14,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   let json = await res.json()
   m.reply(json.message.replace('simsimi', `${botName}`).replace('Simsimi', `${botName}`).replace('sim simi', `${botName}`))
 } catch {
-  m.reply(`❎ Intenta de nuevo mas tarde La api de SimSimi se cayo`)
+  m.reply(lolll.text)
 }
 
 }
